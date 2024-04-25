8c104e22d702bc443b9d3b7264bfa638b85ccbeb
goldenstein64 <goldnstein64@gmail.com>
2024-04-25 01:51:09 -0400

    feat: EasyComputer

---
+++ lib/src/main/java/tic/tac/toe/player/EasyComputer.java
@@ -0,0 +1,25 @@
+package tic.tac.toe.player;
+
+import java.util.Random;
+import java.util.stream.Stream;
+import tic.tac.toe.data.Board;
+import tic.tac.toe.data.Mark;
+
+public class EasyComputer implements Player {
+
+	private Random rng;
+
+	public EasyComputer(Random rng) {
+		this.rng = rng;
+	}
+
+	@Override
+	public int getMove(Board board, Mark mark) {
+		var moves = Stream
+			.iterate(0, i -> i + 1)
+			.limit(Board.SIZE)
+			.filter(board::canMark)
+			.toList();
+		return moves.get(rng.nextInt(moves.size()));
+	}
+}
