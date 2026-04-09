e7522146ee25b40ed72a691a9c9ef7b116b8f7e7
goldenstein64 <goldnstein64@gmail.com>
2026-04-09 17:06:33 -0400

    refactor: make Message records static

--- lib/src/main/java/tic/tac/toe/data/Message.java
+++ lib/src/main/java/tic/tac/toe/data/Message.java
@@ -1,39 +1,39 @@
 package tic.tac.toe.data;
 
 public sealed interface Message {
-	public final record MSG_PromptPlayer(Mark mark) implements Message {
+	public static final record MSG_PromptPlayer(Mark mark) implements Message {
 	}
 
-	public final record MSG_PromptComputer(Mark mark) implements Message {
+	public static final record MSG_PromptComputer(Mark mark) implements Message {
 	}
 
-	public final record MSG_PlayerWon(Mark winner) implements Message {
+	public static final record MSG_PlayerWon(Mark winner) implements Message {
 	}
 
-	public enum StaticMessage implements Message {
+	public static enum StaticMessage implements Message {
 		MSG_Tied
 	}
 
 	public static StaticMessage MSG_Tied = StaticMessage.MSG_Tied;
 
-	public final record MSG_Board(String pattern) implements Message {
+	public static final record MSG_Board(String pattern) implements Message {
 	}
 
-	public final record ERR_PlayerInvalid(String input) implements Message {
+	public static final record ERR_PlayerInvalid(String input) implements Message {
 	}
 
-	public final record ERR_ComputerInvalid(String input) implements Message {
+	public static final record ERR_ComputerInvalid(String input) implements Message {
 	}
 
-	public final record MSG_PromptMove(Mark mark) implements Message {
+	public static final record MSG_PromptMove(Mark mark) implements Message {
 	}
 
-	public final record ERR_NotANumber(String input) implements Message {
+	public static final record ERR_NotANumber(String input) implements Message {
 	}
 
-	public final record ERR_NumberOutOfRange(int choice) implements Message {
+	public static final record ERR_NumberOutOfRange(int choice) implements Message {
 	}
 
-	public final record ERR_SpaceOccupied(int choice) implements Message {
+	public static final record ERR_SpaceOccupied(int choice) implements Message {
 	}
 }
